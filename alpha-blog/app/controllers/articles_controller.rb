0f0d56788f7cff762a42bea7feb1c01786752b51
Nick Iden <iden.nick@gmail.com>
2020-08-31 20:26:45 +0000

    new form added

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -8,6 +8,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   
+  def new
+    
+  end 
+  
+  def create
+    render plain: params[:article]
+  end 
+
+  
     
     
     
