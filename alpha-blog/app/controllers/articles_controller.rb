3e7dcb5c0436c3fa1703eb8f0374a74e12c56b72
Nick Iden <iden.nick@gmail.com>
2020-09-01 20:31:14 +0000

    Display messages and save

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -9,16 +9,21 @@ class ArticlesController < ApplicationController
   end
   
   def new
-    
+    #instatiate article before creating it with form
+    @article = Article.new
   end 
   
   def create
+    #create new instance and allow form info to save
     @article = Article.new(params.require(:article).permit(:title, :description))
-    render plain: @article
+    #save form information to database
+   if @article.save
+     #display sucess notice
+     flash[:notice] = "Article was saved sucessfully!!"
+     redirect_to @article
+   else
+     render 'new'
+   end
   end 
 
-  
-    
-    
-    
 end
