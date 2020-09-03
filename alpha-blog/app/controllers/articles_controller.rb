d86b164cc74124df01229051bea5ec55a64dc014
Nick Iden <iden.nick@gmail.com>
2020-09-03 17:48:40 +0000

    reduncy and software check

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
      
   def show
-    @article = Article.find(params[:id])
+   
   end 
   
   def index
@@ -14,12 +16,12 @@ class ArticlesController < ApplicationController
   end 
   
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def create
     #create new instance and allow form info to save
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     #save form information to database
    if @article.save
      #display sucess notice
@@ -31,8 +33,8 @@ class ArticlesController < ApplicationController
   end 
   
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+   
+    if @article.update(article_params)
       flash[:notice] = "Article was updated sucessfully!!"
       redirect_to @article
       
@@ -42,9 +44,19 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
+   
     @article.destroy
     redirect_to articles_path
   end
+  
+  private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end 
 
 end
