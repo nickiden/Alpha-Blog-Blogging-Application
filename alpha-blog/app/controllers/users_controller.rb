76f8ed07ee6cd6aba97cfc2addc0846fa0dfee54
Nick Iden <iden.nick@gmail.com>
2020-09-09 17:37:11 +0000

    gravatar and pagination added to page

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
     
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end 
+    
+    def index
+        # grab all users
+        @users = User.paginate(page: params[:page], per_page: 5)
     end 
     
     def edit
@@ -17,7 +22,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account has been successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end 
