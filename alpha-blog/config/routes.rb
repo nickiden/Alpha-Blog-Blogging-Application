d185888ca459cfeeb5518ace9fd3b16e5c221347
Nick Iden <iden.nick@gmail.com>
2020-09-11 15:16:53 +0000

    created functional tests and added a category section

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -20,4 +20,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  
+# resorces for categories in db
+  resources :categories, except: [:destroy]
 end
