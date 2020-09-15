451bdf3fc9e5c096c20bc2881a4c2a7f39ccb08e
Nick Iden <iden.nick@gmail.com>
2020-09-14 23:30:55 -0400

    Test to see github is working

--- alpha-blog/test/integration/create_category_test.rb
+++ alpha-blog/test/integration/create_category_test.rb
@@ -13,3 +13,14 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_match "Recording", response.body
   end
 end
+test "get new category form and create category" do
+  get "/categories/new"
+  assert_response :success
+  assert_difference 'Category.count', 1 do
+    post categories_path, params: { category: {name: "Recording"}}
+    assert_response :redirect
+  end
+  follow_redirect!g", response.body
+end
+  assert_response :success
+  assert_match "Recordin
